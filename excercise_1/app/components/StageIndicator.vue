87e4fca75608e4e21f52444a5e3978a8d58fd1fa
Pascal Seifert <seifert.pascal@web.de>
2026-04-19 13:43:08 +0200

    feat: added stage-indicator component

---
+++ excercise_1/app/components/StageIndicator.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="d-flex align-center ga-8">
+    <div
+      v-for="(stage, idx) in STAGES"
+      :key="stage.id"
+      class="d-flex flex-column align-center"
+    >
+      <v-avatar
+        :color="stage.id === currentStage ? 'grey-darken-4' : 'grey'"
+        size="24"
+        @click="onStageClick(stage.id)"
+      >
+        {{ idx + 1 }}
+      </v-avatar>
+
+      <div
+        class="text-body-2"
+        :class="{ 'text-grey-darken-1': stage.id !== currentStage }"
+      >
+        {{ stage.label }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:modelValue", "set-stage"]);
+
+const currentStage = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(newValue) {
+    emit("update:modelValue", newValue);
+  },
+});
+
+const { STAGES } = useDefaults();
+
+function onStageClick(stageId: string) {
+  if (!["INTRO", "RESULTS"].includes(stageId)) return;
+
+  emit("set-stage", stageId);
+}
+</script>
+
+<style scoped></style>
