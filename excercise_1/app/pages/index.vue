7849c97389477c6ea6c2eae179b51d32cd4c73e9
Pascal Seifert <seifert.pascal@web.de>
2026-04-19 13:43:50 +0200

    feat: integrated flow & stages-container on index page

---
+++ excercise_1/app/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-card class="pa-4 mb-4">
+      <div>Visualization SS26 - Excercise 1</div>
+
+      <div>Perceived Area Experiment</div>
+
+      <div>
+        This study estimates the perception exponent <b>x</b> for circles and
+        squares by asking participants to resize a second shape until it appears
+        to match a target ratio.
+      </div>
+    </v-card>
+
+    <v-card class="pa-4 mb-4">
+      <stage-indicator v-model="stage" @set-stage="stage = $event" />
+    </v-card>
+
+    <stages-container v-model="stage" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const stage = ref("INTRO");
+</script>
+
+<style scoped></style>
