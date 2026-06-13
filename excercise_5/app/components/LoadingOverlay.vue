4ac3654ea3a7a0ff6209f2df76b167a6fe22c1a5
Sylver2k <seifert.pascal@web.de>
2026-06-13 15:59:41 +0200

    feat: added loading-overlay component

---
+++ excercise_5/app/components/LoadingOverlay.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-overlay
+    v-model="isLoading"
+    class="align-center justify-center"
+    persistent
+  >
+    <v-progress-circular
+      color="primary"
+      indeterminate
+      size="64"
+      width="6"
+    />
+  </v-overlay>
+</template>
+
+<script setup lang="ts">
+const isLoading = defineModel<boolean>({ default: false });
+</script>
